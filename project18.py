3281649a021991529f1c9abeb19db73bc0ff2307
KianMousavi87 <kianmousavi86@gmail.com>
2024-07-20 22:57:32 +0330

    project18.py

---
+++ project18.py
@@ -0,0 +1,18 @@
+#a = 5
+#print(id(a))
+
+#def func(n):
+#    global a
+#    a = 2
+#    print("a",a)
+#    c = 2 * n
+#    return c
+
+
+#print(func(3))
+#print("a",a)
+
+for i in range(10):
+    x = 2 * i
+
+print(x)
