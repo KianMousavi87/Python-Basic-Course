0291521c07d4d0da3aed3347b96d5cd07c6c6698
KianMousavi87 <kianmousavi86@gmail.com>
2024-07-21 12:23:36 +0330

    project22.py

--- project22.py
+++ project22.py
@@ -2,12 +2,16 @@ class Rectangle:
    
    # __bool__, __str__, __eq__
    # dunder init
-   def __init__(self, width, height):
-    self.width = width
-    self.height = height
+   #constructor
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
 
     def area(self):
         return self.width * self.height
     
 r1 = Rectangle(width=3, height=5)
 print(r1.area())
+
+r2 = Rectangle(width=5, height=4)
+print(r2.area())
